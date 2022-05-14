a5a841a314a0d48c9130c42716c2135838041515
Chukwuyem John <105474703+chukwuyemjohn@users.noreply.github.com>
2022-05-14 07:13:44 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,102 @@
+x = 5
+y = 6
+if x == y:
+  print("i am a King ")
+else:
+  print("i am a Queen")
+
+# ==============
+
+if "hello" not in "hello world!":
+  print("yes")
+else:
+  print("it's in")
+
+x = 15
+y = 15
+
+x = 15
+y = 7
+
+if x > y:
+    print('Yes')
+
+# =====================
+
+x = 5
+y = 7
+
+if x < y:
+  print('No')
+
+if 'Hello' in 'Hello world!':
+    print('yes')
+
+# =====================
+
+x = 7
+y = 7
+
+if x > y:
+    print('yes it is greater')
+elif x < y:
+    print('no it is less')
+else:
+    print('Neither')
+
+# =====================
+# '''
+# Nested if statements
+# '''
+num = -3
+
+if num >= 0:
+    if num == 0:
+        print("Zero")
+    else:
+      print("Positive number")
+else:
+  print('Negative number')
+
+# =====================
+# '''
+# one-line if statements
+# '''
+if 7 > 5: print('yes it is')
+
+# =====================
+# '''
+# Ternary operator
+# '''
+print('Yes') if 7 > 5 else print('No')
+
+if 7 > 5:
+    print('Yes')
+else:
+    print('No')
+
+# =====================
+
+if 100 > 50:
+    print('yah')
+# if x > y:
+#   print("yes x i greater ")
+# elif x < y:
+#   print("no x is less ")
+# else:
+#   print("neither")
+
+# num = 0
+
+# if num >= 0:
+#   if num == 0:
+#     print("zero")
+#   else:
+#     print("positive number")
+# else:
+#   print("negative number")
+print("yes")if 2 > 4 else print("no it's not")
+
+
+
+
